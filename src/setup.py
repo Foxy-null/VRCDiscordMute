7cae8d7819fedc37ef703d03cf304a1240128159
Foxy_null <foxynull@duck.com>
2026-06-23 02:57:29 +0900

    patch for Python 3.13+
    Reformat the executables list into multiple lines for readability and change the second Executable's base from "Win32GUI" to "gui". Also add a newline at EOF. This updates how the GUI build target is specified and cleans up the setup.py formatting.

--- src/setup.py
+++ src/setup.py
@@ -12,5 +12,9 @@ setup(
     version="1.0",
     description="VRCDiscordMute",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", target_name="VRCDiscordMute.exe", base=False), Executable("main.py", target_name="VRCDiscordMute_NoConsole.exe", base="Win32GUI"), Executable("GetHotkeyName.py", target_name="GetHotkeyName.exe", base=False)],
+    executables=[
+        Executable("main.py", target_name="VRCDiscordMute.exe", base=False),
+        Executable("main.py", target_name="VRCDiscordMute_NoConsole.exe", base="gui"),
+        Executable("GetHotkeyName.py", target_name="GetHotkeyName.exe", base=False),
+    ],
 )
